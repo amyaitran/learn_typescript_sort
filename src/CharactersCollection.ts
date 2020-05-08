f198d5e1f3f640bfea629a4ee474bf03ced68b6b
Amy Tran <amyaitran@gmail.com>
2020-05-07 19:46:58 -0700

    add other dependencies

--- src/CharactersCollection.ts
+++ src/CharactersCollection.ts
@@ -3,20 +3,21 @@ export class CharactersCollection {
 
   get length(): number {
     return this.data.length;
-}
+  }
 
-compare(leftIndex: number, rightIndex: number): boolean {
-  return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
-  );
-}
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
 
-swap(leftIndex: number, rightIndex: number): void {
-  const characters = this.data.split('');
+  swap(leftIndex: number, rightIndex: number): void {
+    const characters = this.data.split('');
 
-  const leftHand = characters[leftIndex];
-  characters[leftIndex] = characters[rightIndex];
-  characters[rightIndex] = leftHand;
+    const leftHand = characters[leftIndex];
+    characters[leftIndex] = characters[rightIndex];
+    characters[rightIndex] = leftHand;
 
-  this.data = characters.join('')
+    this.data = characters.join('');
   }
 }
